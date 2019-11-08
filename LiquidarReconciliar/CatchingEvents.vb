2bf646100d3f17a1ad02060739b20381f0b63ed2
raiden16 <siul-luna@hotmail.com>
2019-11-08 10:57:08 -0600

    Inicio

---
+++ LiquidarReconciliar/CatchingEvents.vb
@@ -0,0 +1,243 @@
+﻿Imports System.Windows.Forms
+
+Friend Class CatchingEvents
+
+    Friend WithEvents SBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Friend SBOCompany As SAPbobsCOM.Company '//OBJETO COMPAÑIA
+    Friend csDirectory As String '//DIRECTORIO DONDE SE ENCUENTRAN LOS .SRF
+
+    Public Sub New()
+        MyBase.New()
+        SetAplication()
+        SetConnectionContext()
+        ConnectSBOCompany()
+
+        addMenuItems()
+
+        setFilters()
+
+    End Sub
+
+    '//----- ESTABLECE LA COMUNICACION CON SBO
+    Private Sub SetAplication()
+        Dim SboGuiApi As SAPbouiCOM.SboGuiApi
+        Dim sConnectionString As String
+        Try
+            SboGuiApi = New SAPbouiCOM.SboGuiApi
+            sConnectionString = Environment.GetCommandLineArgs.GetValue(1)
+            SboGuiApi.Connect(sConnectionString)
+            SBOApplication = SboGuiApi.GetApplication()
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con la aplicación SBO " & ex.Message)
+            System.Windows.Forms.Application.Exit()
+            End
+        End Try
+    End Sub
+
+    '//----- ESTABLECE EL CONTEXTO DE LA APLICACION
+    Private Sub SetConnectionContext()
+        Try
+            SBOCompany = SBOApplication.Company.GetDICompany
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con el DI")
+            SBOApplication = Nothing
+            System.Windows.Forms.Application.Exit()
+            End
+            'Finally
+        End Try
+    End Sub
+
+    '//----- CONEXION CON LA BASE DE DATOS
+    Private Sub ConnectSBOCompany()
+        Dim loRecSet As SAPbobsCOM.Recordset
+        Try
+            '//ESTABLECE LA CONEXION A LA COMPAÑIA
+            csDirectory = My.Application.Info.DirectoryPath
+            If (csDirectory = "") Then
+                System.Windows.Forms.Application.Exit()
+                End
+            End If
+        Catch ex As Exception
+            SBOApplication.MessageBox("Falló la conexión con la BD. " & ex.Message)
+            SBOApplication = Nothing
+            System.Windows.Forms.Application.Exit()
+            End
+        Finally
+            loRecSet = Nothing
+        End Try
+    End Sub
+
+
+    Private Sub addMenuItems()
+        Dim loForm As SAPbouiCOM.Form = Nothing
+        Dim loMenus As SAPbouiCOM.Menus
+        Dim loMenusRoot As SAPbouiCOM.Menus
+        Dim loMenuItem As SAPbouiCOM.MenuItem
+
+        Try
+            '////// Obtiene referencia de la forma Principal de Modulos
+            loForm = SBOApplication.Forms.GetForm(169, 1)
+
+            loForm.Freeze(True)
+
+            '////// Obtiene la referencia de los Menus de SBO
+            loMenus = SBOApplication.Menus.Item(6).SubMenus
+
+            '////// Adiciona un Nuevo Menu para la Aplicacion de VectorSBO
+            If loMenus.Exists("REC01") Then
+                loMenus.RemoveEx("REC01")
+            End If
+
+            loMenuItem = loMenus.Add("REC01", "Liquidador de Saldos", SAPbouiCOM.BoMenuType.mt_POPUP, loMenus.Count)
+
+            loMenusRoot = loMenuItem.SubMenus
+
+            '////// Adiciona un menu Item
+            If loMenusRoot.Exists("REC11") Then
+                loMenusRoot.RemoveEx("REC11")
+            End If
+            loMenuItem = loMenusRoot.Add("REC11", "Liquidar Saldos", SAPbouiCOM.BoMenuType.mt_STRING, loMenusRoot.Count)
+            loMenus = loMenuItem.SubMenus
+
+            loForm.Freeze(False)
+            loForm.Update()
+
+        Catch ex As Exception
+            If (Not loForm Is Nothing) Then
+                loForm.Freeze(False)
+                loForm.Update()
+            End If
+            SBOApplication.MessageBox("CatchingEvents. Error al agregar las opciones del menú. " & ex.Message)
+            End
+        Finally
+            loMenus = Nothing
+            loMenusRoot = Nothing
+            loMenuItem = Nothing
+        End Try
+    End Sub
+
+
+    '//----- ESTABLECE FILTROS DE EVENTOS DE LA APLICACION
+    Private Sub setFilters()
+        Dim lofilter As SAPbouiCOM.EventFilter
+        Dim lofilters As SAPbouiCOM.EventFilters
+
+        Try
+
+            lofilters = New SAPbouiCOM.EventFilters
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED)
+            lofilter.AddEx("tekDelivery") '////// FORMA UDO DE ENTREGAS
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_KEY_DOWN)
+            lofilter.AddEx("tekDelivery") '////// FORMA UDO DE ENTREGAS
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_COMBO_SELECT)
+            lofilter.AddEx("tekDelivery") '////// FORMA UDO DE ENTREGAS
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_MENU_CLICK)
+
+            SBOApplication.SetFilter(lofilters)
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("SetFilter: " & ex.Message)
+        End Try
+
+    End Sub
+
+
+    ''//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ''// METODOS PARA EVENTOS DE LA APLICACION
+    ''//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Private Sub SBO_Application_AppEvent(ByVal EventType As SAPbouiCOM.BoAppEventTypes) Handles SBOApplication.AppEvent
+        Select Case EventType
+            Case SAPbouiCOM.BoAppEventTypes.aet_ShutDown, SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition, SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged
+                System.Windows.Forms.Application.Exit()
+                End
+        End Select
+
+    End Sub
+
+
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    '// CONTROLADOR DE EVENTOS MENU
+    '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Private Sub SBOApplication_MenuEvent(ByRef pVal As SAPbouiCOM.MenuEvent, ByRef BubbleEvent As Boolean) Handles SBOApplication.MenuEvent
+        Dim otekDel As FrmtekDel
+
+        Try
+            '//ANTES DE PROCESAR SBO
+            If pVal.BeforeAction = False Then
+                Select Case pVal.MenuUID
+                    '//////////////////////////////////SubMenu de Crear traslado inventario////////////////////////
+                    Case "DEL11"
+
+                        otekDel = New FrmtekDel
+                        otekDel.openForm(csDirectory)
+
+                End Select
+            End If
+
+        Catch ex As Exception
+            SBOApplication.MessageBox("clsCatchingEvents. MenuEvent " & ex.Message)
+        Finally
+            'oReservaPedido = Nothing
+        End Try
+    End Sub
+
+
+    Private Sub SBOApplication_AppEvent(ByVal EventType As SAPbouiCOM.BoAppEventTypes) Handles SBOApplication.AppEvent
+        Select Case EventType
+            Case SAPbouiCOM.BoAppEventTypes.aet_ShutDown, SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition, SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged
+                System.Windows.Forms.Application.Exit()
+                End
+        End Select
+    End Sub
+
+
+    'Private Sub SBOApplication_ItemEvent(ByVal FormUID As String, ByRef pVal As SAPbouiCOM.ItemEvent, ByRef BubbleEvent As Boolean) Handles SBOApplication.ItemEvent
+    '    Try
+    '        If pVal.Before_Action = False And pVal.FormTypeEx <> "" Then
+
+    '            Select Case pVal.FormTypeEx
+    '                '////////////////FORMA PARA ACTIVAR LICENCIA
+    '                Case "tekDelivery"
+    '                    FrmEntregaSBOControllerAfter(FormUID, pVal)
+    '            End Select
+    '        End If
+
+    '    Catch ex As Exception
+    '        SBOApplication.MessageBox("SBOApplication_ItemEvent. ItemEvent " & ex.Message)
+    '    Finally
+    '    End Try
+    'End Sub
+
+    Public Function addDelivery(ByVal FormUID As String, ByVal csDirectory As String)
+
+        Dim oReconService As SAPbobsCOM.InternalReconciliationsService = SBOCompany.GetCompanyService().GetBusinessService(SAPbobsCOM.ServiceTypes.InternalReconciliationsService)
+        Dim openTrans As SAPbobsCOM.InternalReconciliationOpenTrans = oReconService.GetDataInterface(SAPbobsCOM.InternalReconciliationsServiceDataInterfaces.irsInternalReconciliationOpenTrans)
+        Dim reconParams As SAPbobsCOM.InternalReconciliationParams = oReconService.GetDataInterface(SAPbobsCOM.InternalReconciliationsServiceDataInterfaces.irsInternalReconciliationParams)
+
+        For i = 0 To 10 'gridRecon.DataTable.Rows.Count - 1
+
+
+            If gridRecon.DataTable.GetValue("Select", gridRecon.GetDataTableRowIndex(i)) = "Y" Then
+
+                SBOApplication.SetStatusBarMessage("Reconcilling Please wait...", SAPbouiCOM.BoMessageTime.bmt_Short, False) With openTrans 'For Incoming Payment '1st Line 
+
+            If gridRecon.DataTable.GetValue("Type", gridRecon.GetDataTableRowIndex(i)) = "RC" Then
+                    .InternalReconciliationOpenTransRows.Add()
+                    .InternalReconciliationOpenTransRows.Item(x).Selected = SAPbobsCOM.BoYesNoEnum.tYES
+                    .InternalReconciliationOpenTransRows.Item(x).TransId = gridRecon.DataTable.GetValue("TransId", gridRecon.GetDataTableRowIndex(i)) ' Journal Entry ID: TransId in OJDT 
+                    .InternalReconciliationOpenTransRows.Item(x).TransRowId = 1 ' Journal Entry Line Number: Line_ID in JDT1 
+                    oIncomPayment = Math.Abs(gridRecon.DataTable.GetValue("Actual Amount", gridRecon.GetDataTableRowIndex(i))) ' MsgBox(oIncomPayment) 'oTotal = oARPayment - oIncomPayment 
+                    .InternalReconciliationOpenTransRows.Item(x).ReconcileAmount = oARPayment 'gridRecon.DataTable.GetValue("Actual Amount", gridRecon.GetDataTableRowIndex(i)) 
+                    ' This should always be positive value. But one line should be on Credit, one line is Debit. 
+                    oTransId = gridRecon.DataTable.GetValue("TransId", gridRecon.GetDataTableRowIndex(i)) ' 
+                ElseIf gridRecon.DataTable.GetValue("Type", gridRecon.GetDataTableRowIndex(i)) = "JE" Then
+                Else openTrans.CardOrAccount = SAPbobsCOM.CardOrAccountEnum.coaCard .InternalReconciliationOpenTransRows.Add() .InternalReconciliationOpenTransRows.Item(x).Selected = SAPbobsCOM.BoYesNoEnum.tYES .InternalReconciliationOpenTransRows.Item(x).TransId = gridRecon.DataTable.GetValue("TransId", gridRecon.GetDataTableRowIndex(i)) .InternalReconciliationOpenTransRows.Item(x).TransRowId = 0 ' Journal Entry Line Number: Line_ID in JDT1 .InternalReconciliationOpenTransRows.Item(x).ReconcileAmount = Math.Abs(gridRecon.DataTable.GetValue("Actual Amount", gridRecon.GetDataTableRowIndex(i))) ' This should always be positive value. But one line should be on Credit, one line is Debit. Console.WriteLine(Math.Abs(gridRecon.DataTable.GetValue("Actual Amount", gridRecon.GetDataTableRowIndex(i)))) If gridRecon.DataTable.GetValue("Type", gridRecon.GetDataTableRowIndex(i)) = "JE" Then oJeTranID = gridRecon.DataTable.GetValue("TransId", gridRecon.GetDataTableRowIndex(i)) olistPostedJE.Add(oJeTranID) End If End If If Not gridRecon.DataTable.GetValue("Type", gridRecon.GetDataTableRowIndex(i)) = "RC" Then oARPayment += Math.Abs(gridRecon.DataTable.GetValue("Actual Amount", i)) End If x = x + 1 End With End If 
+
+        Next
+
+        Try reconParams = oReconService.Add(openTrans) Catch ex As Exception SAP_APP.SetMessage(ex.ToString, SAPbouiCOM.BoStatusBarMessageType.smt_Error) oWriteText(Now & " - " & "[Err] - " & ex.ToString, True) oCompany.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_RollBack) Return False End Try Try oCompany.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_Commit) Catch ex As Exception End Try
+
+    End Function
+
+End Class
---
+++ LiquidarReconciliar/FrmtekLIQ.vb
@@ -0,0 +1,260 @@
+﻿Imports System.Drawing
+
+Public Class FrmtekLIQ
+
+    Private cSBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Private cSBOCompany As SAPbobsCOM.Company     '//OBJETO DE CONEXION
+    Private coForm As SAPbouiCOM.Form           '//FORMA
+    Private csFormUID As String
+    Private stDocNum As String
+    Friend Monto As Double
+
+
+    '//----- METODO DE CREACION DE LA CLASE
+    Public Sub New()
+        MyBase.New()
+        cSBOApplication = oCatchingEvents.SBOApplication
+        cSBOCompany = oCatchingEvents.SBOCompany
+        Me.stDocNum = stDocNum
+    End Sub
+
+    'Private Property stRuta As String
+
+    '//----- ABRE LA FORMA DENTRO DE LA APLICACION
+    Public Function openForm(ByVal psDirectory As String)
+        Dim stQueryH As String
+        Dim oRecSetH As SAPbobsCOM.Recordset
+        'Dim Monto As Integer
+
+        Try
+
+            oRecSetH = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+
+            csFormUID = "tekReconciliation"
+            '//CARGA LA FORMA
+            If (loadFormXML(cSBOApplication, csFormUID, psDirectory + "\Forms\" + csFormUID + ".srf") <> 0) Then
+
+                Err.Raise(-1, 1, "")
+            End If
+            '"que pedo"
+            '--- Referencia de Forma
+            setForm(csFormUID)
+
+            cargarComboChofi()
+
+            AgregarLineas()
+
+            '---- refresca forma
+            coForm.Refresh()
+            coForm.Visible = True
+
+            coForm = cSBOApplication.Forms.Item("tekDelivery")
+            coForm.DataSources.UserDataSources.Item("dsUser").Value = cSBOCompany.UserName
+            coForm.DataSources.UserDataSources.Item("dsDate").Value = Now.Date
+            coForm.DataSources.UserDataSources.Item("dsTruck").Value = ""
+
+            stQueryH = "Select count(""Code"")+1 as ""DocEntry"" from ""@EP_EN0"""
+            oRecSetH.DoQuery(stQueryH)
+
+            coForm.DataSources.UserDataSources.Item("dsDocN").Value = oRecSetH.Fields.Item("DocEntry").Value
+
+            coForm.Items.Item("2").Enabled = False
+            coForm.Items.Item("4").Enabled = False
+            coForm.Items.Item("5").Enabled = False
+
+            Return Monto
+
+        Catch ex As Exception
+            If (ex.Message <> "") Then
+                cSBOApplication.MessageBox("FrmtekDel. No se pudo iniciar la forma. " & ex.Message)
+            End If
+            Me.close()
+        End Try
+    End Function
+
+
+    '//----- CIERRA LA VENTANA
+    Public Function close() As Integer
+        close = 0
+        coForm.Close()
+    End Function
+
+
+    '//----- ABRE LA FORMA DENTRO DE LA APLICACION
+    Public Function setForm(ByVal psFormUID As String) As Integer
+        Try
+            setForm = 0
+            '//ESTABLECE LA REFERENCIA A LA FORMA
+            coForm = cSBOApplication.Forms.Item(psFormUID)
+            '//OBTIENE LA REFERENCIA A LOS USER DATA SOURCES
+            setForm = getUserDataSources()
+        Catch ex As Exception
+            cSBOApplication.MessageBox("FrmtekDel. Al referenciar a la forma. " & ex.Message)
+            setForm = -1
+        End Try
+    End Function
+
+
+    '//----- OBTIENE LA REFERENCIA A LOS USERDATASOURCES
+    Private Function getUserDataSources() As Integer
+        'Dim llIndice As Integer
+        Try
+            coForm.Freeze(True)
+            getUserDataSources = 0
+            '//SI YA EXISTEN LOS DATASOURCES, SOLO LOS ASOCIA
+            If (coForm.DataSources.UserDataSources.Count() > 0) Then
+            Else '//EN CASO DE QUE NO EXISTAN, LOS CREA
+                getUserDataSources = bindUserDataSources()
+            End If
+            coForm.Freeze(False)
+        Catch ex As Exception
+            cSBOApplication.MessageBox("FrmtekDel. Al referenciar los UserDataSources" & ex.Message)
+            getUserDataSources = -1
+        End Try
+    End Function
+
+
+    '//----- ASOCIA LOS USERDATA A ITEMS
+    Private Function bindUserDataSources() As Integer
+        Dim loText As SAPbouiCOM.EditText
+        Dim loDS As SAPbouiCOM.UserDataSource
+        Dim oDataTable As SAPbouiCOM.DataTable
+        Dim oGrid As SAPbouiCOM.Grid
+        Dim oCombo As SAPbouiCOM.ComboBox
+
+        Try
+            bindUserDataSources = 0
+
+            loDS = coForm.DataSources.UserDataSources.Add("dsDriver", SAPbouiCOM.BoDataType.dt_SHORT_TEXT) 'Creo el datasources
+            oCombo = coForm.Items.Item("1").Specific  'identifico mi combobox
+            oCombo.DataBind.SetBound(True, "", "dsDriver")   ' uno mi userdatasources a mi combobox
+
+            loDS = coForm.DataSources.UserDataSources.Add("dsUser", SAPbouiCOM.BoDataType.dt_SHORT_TEXT) 'Creo el datasources
+            loText = coForm.Items.Item("2").Specific  'identifico mi caja de texto
+            loText.DataBind.SetBound(True, "", "dsUser")   ' uno mi userdatasources a mi caja de texto
+
+            loDS = coForm.DataSources.UserDataSources.Add("dsTruck", SAPbouiCOM.BoDataType.dt_SHORT_TEXT) 'Creo el datasources
+            loText = coForm.Items.Item("3").Specific  'identifico mi caja de texto
+            loText.DataBind.SetBound(True, "", "dsTruck")   ' uno mi userdatasources a mi caja de texto
+
+            loDS = coForm.DataSources.UserDataSources.Add("dsDocN", SAPbouiCOM.BoDataType.dt_SHORT_TEXT) 'Creo el datasources
+            loText = coForm.Items.Item("4").Specific  'identifico mi caja de texto
+            loText.DataBind.SetBound(True, "", "dsDocN")   ' uno mi userdatasources a mi caja de texto
+
+            loDS = coForm.DataSources.UserDataSources.Add("dsDate", SAPbouiCOM.BoDataType.dt_SHORT_TEXT) 'Creo el datasources
+            loText = coForm.Items.Item("5").Specific  'identifico mi caja de texto
+            loText.DataBind.SetBound(True, "", "dsDate")   ' uno mi userdatasources a mi caja de texto
+
+            oGrid = coForm.Items.Item("11").Specific
+            oDataTable = coForm.DataSources.DataTables.Add("Invoices")
+            oGrid.DataTable = oDataTable
+
+        Catch ex As Exception
+            cSBOApplication.MessageBox("FrmtekDel. Al crear los UserDataSources. " & ex.Message)
+            bindUserDataSources = -1
+        Finally
+            loText = Nothing
+            loDS = Nothing
+            oDataTable = Nothing
+            oGrid = Nothing
+        End Try
+    End Function
+
+
+    '---- Carga de Porcentajes
+    Public Function cargarComboChofi()
+
+        Dim oCombo As SAPbouiCOM.ComboBox
+        Dim oRecSet As SAPbobsCOM.Recordset
+
+        Try
+            cargarComboChofi = 0
+            '--- referencia de combo 
+            oCombo = coForm.Items.Item("1").Specific
+            coForm.Freeze(True)
+            '---- SI YA SE TIENEN VALORES, SE ELIMMINAN DEL COMBO
+            If oCombo.ValidValues.Count > 0 Then
+                Do
+                    oCombo.ValidValues.Remove(0, SAPbouiCOM.BoSearchKey.psk_Index)
+                Loop While oCombo.ValidValues.Count > 0
+            End If
+            '--- realizar consulta
+            oRecSet = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+            oRecSet.DoQuery(" Select null as ""Name"",null as ""Code"" from dummy union all Select ""Name"",""Code"" from ""@EP_EN2""")
+            '---- cargamos resultado
+            oRecSet.MoveFirst()
+            Do While oRecSet.EoF = False
+                oCombo.ValidValues.Add(oRecSet.Fields.Item(0).Value, oRecSet.Fields.Item(1).Value)
+                oRecSet.MoveNext()
+            Loop
+            oCombo.Select(0, SAPbouiCOM.BoSearchKey.psk_Index)
+            coForm.Freeze(False)
+
+
+        Catch ex As Exception
+            coForm.Freeze(False)
+            MsgBox("FrmtekDel. Fallo la carga previa del comboBox cargarComboChofi: " & ex.Message)
+        Finally
+            oCombo = Nothing
+            oRecSet = Nothing
+        End Try
+    End Function
+
+
+    '----- carga los procesos de carga
+    Public Function AgregarLineas()
+        Dim oGrid As SAPbouiCOM.Grid
+        Dim stQuery As String = ""
+        Dim oRecSet As SAPbobsCOM.Recordset
+        Dim oCombo As SAPbouiCOM.ComboBoxColumn
+
+        Try
+
+            oGrid = coForm.Items.Item("11").Specific
+            oGrid.DataTable.Clear()
+
+            oRecSet = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+            stQuery = "Select 1 as ""#"",'                         ' as ""Factura"", '          ' as ""Fecha Factura"", '          ' as ""Fecha Escaneo"", '          ' as ""Estatus"" from dummy"
+            oGrid.DataTable.ExecuteQuery(stQuery)
+
+            oGrid.Columns.Item(4).Type = SAPbouiCOM.BoGridColumnType.gct_ComboBox
+            oCombo = oGrid.Columns.Item(4)
+
+            oCombo.ValidValues.Add("", "")
+            oCombo.ValidValues.Add("Escaneado", "Escaneado")
+            oCombo.ValidValues.Add("Retenido", "Retenido")
+            oCombo.ValidValues.Add("Cancelado", "Cancelado")
+
+            oGrid.Columns.Item(1).Editable = True
+            oGrid.Columns.Item(2).Editable = True
+            oGrid.Columns.Item(3).Editable = True
+
+            oGrid.DataTable.Rows.Add(19)
+
+            For i = 1 To 19
+                oGrid.DataTable.SetValue("#", i, i + 1)
+            Next
+
+            ' Set columns size
+            oGrid.Columns.Item(0).Width = 30
+            oGrid.Columns.Item(1).Width = 100
+            oGrid.Columns.Item(2).Width = 100
+            oGrid.Columns.Item(3).Width = 100
+            oGrid.Columns.Item(4).Width = 100
+            oGrid.Columns.Item(0).Editable = False
+
+            Return 0
+
+        Catch ex As Exception
+
+            MsgBox("FrmtekDel. fallo la carga previa de la forma AgregarLineas: " & ex.Message)
+
+        Finally
+
+            oGrid = Nothing
+
+        End Try
+
+    End Function
+
+End Class
